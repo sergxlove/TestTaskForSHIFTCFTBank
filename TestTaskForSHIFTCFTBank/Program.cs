2bdff6aadbbb02780b73f69372fee02952767fdd
sergxlove <sergxlove@mail.ru>
2025-12-10 21:52:30 +0600

    Добавьте файлы проекта.

---
+++ TestTaskForSHIFTCFTBank/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestTaskForSHIFTCFTBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
