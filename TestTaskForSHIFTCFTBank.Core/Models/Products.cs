f1a7efe10a6f2a4111b267f967316b8fac64911e
sergxlove <sergxlove@mail.ru>
2025-12-11 19:24:52 +0600

    add models tables

---
+++ TestTaskForSHIFTCFTBank.Core/Models/Products.cs
@@ -0,0 +1,12 @@
+﻿namespace TestTaskForSHIFTCFTBank.Core.Models
+{
+    public class Products
+    {
+        public int ID { get; set; }
+        public int PRODUCTS_TYPE_ID { get; set; }
+        public string NAME { get; set; } = string.Empty;
+        public int CLIENT_REF { get; set; }
+        public DateOnly OPEN_DATE { get; set; }
+        public DateOnly CLOSE_DATE { get; set; }
+    }
+}
---
+++ TestTaskForSHIFTCFTBank.Core/Models/Records.cs
@@ -0,0 +1,11 @@
+﻿namespace TestTaskForSHIFTCFTBank.Core.Models
+{
+    public class Records
+    {
+        public int ID { get; set; }
+        public bool DT { get; set; }
+        public decimal SUM { get; set; }
+        public string ACC_REF { get; set; } = string.Empty;
+        public DateOnly OPER_DATE { get; set; }
+    }
+}
