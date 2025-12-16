8d70356ff7439069ca86c83f7e03cc840302361e
sergxlove <sergxlove@mail.ru>
2025-12-16 22:07:07 +0600

    add meethods for clear data tables

---
+++ TestTaskForSHIFTCFTBank.DataAccess.Postgres/Scripts/DeleteDataTableScripts.cs
@@ -0,0 +1,57 @@
+﻿namespace TestTaskForSHIFTCFTBank.DataAccess.Postgres.Scripts
+{
+    public class DeleteDataTableScripts
+    {
+        public static string DeleteTarifs()
+        {
+            return """
+                DELETE * FROM tarifs;
+                """;
+        }
+
+        public static string DeleteProductType()
+        {
+            return """
+                DELETE * FROM productype;
+                """;
+        }
+
+        public static string DeleteClients()
+        {
+            return """
+                DELETE * FROM clients;
+                """;
+        }
+
+        public static string DeleteProducts()
+        {
+            return """
+                DELETE * FROM products;
+                """;
+        }
+
+        public static string DeleteAccounts()
+        {
+            return """
+                DELETE * FROM accounts;
+                """;
+        }
+
+        public static string DeleteRecords()
+        {
+            return """
+                DELETE * FROM records;
+                """;
+        }
+
+        public static string FieldAll()
+        {
+            return DeleteRecords() + "\n" +
+                DeleteAccounts() + "\n" +
+                DeleteProducts() + "\n" +
+                DeleteClients() + "\n" +
+                DeleteProductType() + "\n" +
+                DeleteTarifs();
+        }
+    }
+}
